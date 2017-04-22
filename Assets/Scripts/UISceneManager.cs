4423bf3ae6d1bee936ce01685858b8f7d0ba1ed5
Miguel Penalosa <guelpenalosa03@gmail.com>
2017-04-22 21:27:50 +0800

    Updated
    Added UI.

---
+++ Assets/Scripts/UISceneManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ using UnityEngine.SceneManagement;
+public class UISceneManager : MonoBehaviour {
+
+    // Use this for initialization
+    public string LevelToLoad;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void LoadThis()
+    {
+        SceneManager.LoadScene(LevelToLoad);
+    }
+}
